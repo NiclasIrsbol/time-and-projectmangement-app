28f88e1daa7dacae59162e736c1ec11f10200725
Niclas <98100476+NiclasIrsbol@users.noreply.github.com>
2026-01-01 22:29:05 +0100

    Delete 02161 eksamensprojekt (Time and projectmangement app)/software-engineering-project/Software_Engineering_Project directory

--- 02161 eksamensprojekt (Time and projectmangement app)/software-engineering-project/Software_Engineering_Project/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module hellofx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens business to javafx.fxml; // Gives access to fxml files
-    exports business; // Exports the class inheriting from javafx.application.Application
-}
